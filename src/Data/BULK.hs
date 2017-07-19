b3e7f5ef1a0a7ba57db2acd3667fad959bb6e4fe
Pierre Thierry <pierre@nothos.net>
2017-07-19 08:01:34 +0200

    Typos

--- src/Data/BULK.hs
+++ src/Data/BULK.hs
@@ -7,11 +7,11 @@ import qualified Data.ByteString as BS
 data BULK = Nil
           | Form [BULK]
           | Array BS.ByteString
-          | UnsigneWord8 Word8
-          | UnsigneWord16 Word16
-          | UnsigneWord32 Word32
-          | UnsigneWord64 Word64
-          | UnsigneWord128 Word128
+          | UnsignedWord8 Word8
+          | UnsignedWord16 Word16
+          | UnsignedWord32 Word32
+          | UnsignedWord64 Word64
+          | UnsignedWord128 Word128
           | SignedWord8 Word8
           | SignedWord16 Word16
           | SignedWord32 Word32
