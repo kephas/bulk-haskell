176fef597648086643ac6e6892aa9bfda12e003b
Pierre Thierry <pierrenothos.net>
2022-03-27 13:44:29 +0200

    Remove warnings

--- test/Numbers.hs
+++ test/Numbers.hs
@@ -5,6 +5,10 @@ import           Test.Hspec.QuickCheck
 import           Test.QuickCheck
 import           Utils
 
+pow256 :: Integer -> Integer
+pow256 n = 256 ^ n
+
+test_number_decoding :: SpecWith ()
 test_number_decoding = describe "reads numbers" $ do
   prop "reads 8bits words" $ forAll arbitraryByte $ \num ->
     (toNums $ readBin [1, 4, num, 2]) `shouldBe` [num]
@@ -15,14 +19,14 @@ test_number_decoding = describe "reads numbers" $ do
     $ forAll (vectorOf 2 arbitraryByte)
     $ \[num1, num2] ->
         (toNums $ readBin [1, 5, num1, num2, 2])
-          `shouldBe` [(fromIntegral num1) * 256 + (fromIntegral num2)]
+          `shouldBe` [(fromIntegral num1) * pow256 1 + (fromIntegral num2)]
   prop "reads 32bits words"
     $ forAll (vectorOf 4 arbitraryByte)
     $ \[num1, num2, num3, num4] ->
         (toNums $ readBin [1, 6, num1, num2, num3, num4, 2])
-          `shouldBe` [ ((fromIntegral num1) * 256 * 256 * 256)
-                       + ((fromIntegral num2) * 256 * 256)
-                       + ((fromIntegral num3) * 256)
+          `shouldBe` [ ((fromIntegral num1) * pow256 3)
+                       + ((fromIntegral num2) * pow256 2)
+                       + ((fromIntegral num3) * pow256 1)
                        + (fromIntegral num4)
                      ]
   prop "reads 64bits words"
@@ -31,28 +35,13 @@ test_number_decoding = describe "reads numbers" $ do
         ( toNums
           $ readBin [1, 7, num1, num2, num3, num4, num5, num6, num7, num8, 2]
           )
-          `shouldBe` [ ( (fromIntegral num1)
-                       * 256
-                       * 256
-                       * 256
-                       * 256
-                       * 256
-                       * 256
-                       * 256
-                       )
-                       + ( (fromIntegral num2)
-                         * 256
-                         * 256
-                         * 256
-                         * 256
-                         * 256
-                         * 256
-                         )
-                       + ((fromIntegral num3) * 256 * 256 * 256 * 256 * 256)
-                       + ((fromIntegral num4) * 256 * 256 * 256 * 256)
-                       + ((fromIntegral num5) * 256 * 256 * 256)
-                       + ((fromIntegral num6) * 256 * 256)
-                       + ((fromIntegral num7) * 256)
+          `shouldBe` [ ((fromIntegral num1) * pow256 7)
+                       + ((fromIntegral num2) * pow256 6)
+                       + ((fromIntegral num3) * pow256 5)
+                       + ((fromIntegral num4) * pow256 4)
+                       + ((fromIntegral num5) * pow256 3)
+                       + ((fromIntegral num6) * pow256 2)
+                       + ((fromIntegral num7) * pow256 1)
                        + (fromIntegral num8)
                      ]
   prop "reads 128bits words"
@@ -80,20 +69,20 @@ test_number_decoding = describe "reads numbers" $ do
             , 2
             ]
           )
-          `shouldBe` [ ((fromIntegral num0) * 256 ^ 15)
-                       + ((fromIntegral num1) * 256 ^ 14)
-                       + ((fromIntegral num2) * 256 ^ 13)
-                       + ((fromIntegral num3) * 256 ^ 12)
-                       + ((fromIntegral num4) * 256 ^ 11)
-                       + ((fromIntegral num5) * 256 ^ 10)
-                       + ((fromIntegral num6) * 256 ^ 9)
-                       + ((fromIntegral num7) * 256 ^ 8)
-                       + ((fromIntegral num8) * 256 ^ 7)
-                       + ((fromIntegral num9) * 256 ^ 6)
-                       + ((fromIntegral numA) * 256 ^ 5)
-                       + ((fromIntegral numB) * 256 ^ 4)
-                       + ((fromIntegral numC) * 256 ^ 3)
-                       + ((fromIntegral numD) * 256 ^ 2)
-                       + ((fromIntegral numE) * 256)
+          `shouldBe` [ ((fromIntegral num0) * pow256 15)
+                       + ((fromIntegral num1) * pow256 14)
+                       + ((fromIntegral num2) * pow256 13)
+                       + ((fromIntegral num3) * pow256 12)
+                       + ((fromIntegral num4) * pow256 11)
+                       + ((fromIntegral num5) * pow256 10)
+                       + ((fromIntegral num6) * pow256 9)
+                       + ((fromIntegral num7) * pow256 8)
+                       + ((fromIntegral num8) * pow256 7)
+                       + ((fromIntegral num9) * pow256 6)
+                       + ((fromIntegral numA) * pow256 5)
+                       + ((fromIntegral numB) * pow256 4)
+                       + ((fromIntegral numC) * pow256 3)
+                       + ((fromIntegral numD) * pow256 2)
+                       + ((fromIntegral numE) * pow256 1)
                        + (fromIntegral numF)
                      ]
