2426cae369e6b59c06c136cc15ac5677873a6fb9
Pierre Thierry <pierre@nothos.net>
2026-01-31 02:15:49 +0100

    Refactor tests
    Make literal bytestrings more readable

--- test/Spec.hs
+++ test/Spec.hs
@@ -7,7 +7,7 @@
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TypeApplications #-}
 
-import Data.ByteString.Lazy (ByteString, pack, singleton)
+import Data.ByteString.Lazy (ByteString, fromStrict, pack, singleton)
 import Data.Foldable (for_, traverse_)
 import Data.Function (on)
 import Data.List (nubBy)
@@ -16,6 +16,7 @@ import Data.Word (Word8)
 import Test.Hspec
 import Test.Hspec.QuickCheck (prop)
 import Test.QuickCheck (arbitrary, chooseInt, forAll, listOf)
+import Text.Hex qualified as H
 import Witch (from, via)
 import Prelude hiding (readFile)
 
@@ -23,6 +24,7 @@ import Data.BULK
 import Data.BULK.Encode (pattern IntReference)
 import Data.BULK.Eval (mkContext)
 import Data.BULK.Types (pattern Core)
+import Data.Maybe (fromJust)
 import Data.Text (Text)
 import Test.BULK.Decode
 import Test.QuickCheck.Instances.BULK (simpleBULK)
@@ -89,8 +91,8 @@ spec = describe "BULK" $ do
                 encode [Nil, Form [], Array "", IntReference 16 0] `shouldBe` "\x00\x01\x02\xC0\x10\x00"
             it "encodes natural numbers" $ do
                 map @Int encodeNat [0, 1, 0xFF, 0x100, 0xFFFF, 0x1_0000] `shouldBe` [Array "\0", Array "\1", Array "\xFF", Array "\1\0", Array "\xFF\xFF", Array "\0\1\0\0"]
-                map @Integer encodeNat [0xFFFF_FFFF_FFFF_FFFF_FFFF_FFFF] `shouldBe` [Array "\x00\x00\x00\x00\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF"]
-                encode [Array "\0", Array "\1", Array "\xFF", Array "\1\0"] `shouldBe` "\x80\x81\xC1\xFF\xC2\x01\x00"
+                map @Integer encodeNat [0xFFFF_FFFF_FFFF_FFFF_FFFF_FFFF] `shouldBe` [Array $ fromHex "00000000FFFFFFFFFFFFFFFFFFFFFFFF"]
+                encode [Array "\0", Array "\1", Array "\xFF", Array "\1\0"] `shouldBe` fromHex "8081C1FFC20100"
             prop "round-trips arbitrary primitives" $ \expr ->
                 encode [expr] `shouldParseTo` expr
         --
@@ -107,7 +109,7 @@ spec = describe "BULK" $ do
                 "#[0]" `shouldDenote` [Array ""]
                 "#[1] 0xAB" `shouldDenote` [Array "\xAB"]
                 "#[2] 0xCDEF" `shouldDenote` [Array "\xCD\xEF"]
-                "#[16] 0xaf5ac1b8-8e33-4025-97fe-f0e2030a00f7" `shouldDenote` [Array "\xaf\x5a\xc1\xb8\x8e\x33\x40\x25\x97\xfe\xf0\xe2\x03\x0a\x00\xf7"]
+                "#[16] 0xaf5ac1b8-8e33-4025-97fe-f0e2030a00f7" `shouldDenote` [Array $ fromHex "af5ac1b88e33402597fef0e2030a00f7"]
             it "parses small decimals" $ do
                 for_ smallWords $ \word ->
                     [i|#{word}|] `shouldDenote` [Array $ singleton word]
@@ -191,7 +193,7 @@ spec = describe "BULK" $ do
                 matchTo [([True, False], Form [Core 0x01, Core 0x02])]
                 matchTo @ByteString [("", Array ""), ("\0\1\2", Array "\0\1\2")]
             it "encodes UTF-8 strings" $ do
-                matchTo @Text [("foo", Array "foo"), ("γράφω", Array "\xCE\xB3\xCF\x81\xCE\xAC\xCF\x86\xCF\x89")]
+                matchTo @Text [("foo", Array "foo"), ("γράφω", Array $ fromHex "CEB3CF81CEACCF86CF89")]
 
     describe "slow tests" $ do
         prop "reads really big generic arrays" $ test_bigger_arrays_decoding 3
@@ -255,7 +257,7 @@ bigIntCases =
 hash0 :: NamespaceDefinition
 hash0 =
     NamespaceDefinition
-        { matchID = MatchNamePrefix 0x00 "\xE2\xEC\xDA\x49\x4A\x78\x19\x5B\x07\x03\x4A\xB1\x0B\x2C\x43\x90\xC9\x4C\x01\x25\x1B\x13\xD1\xA4\x83\xD2\x1E\x8B\x78\x1D\x70\x3D"
+        { matchID = MatchNamePrefix 0x00 $ fromHex "E2ECDA494A78195B07034AB10B2C4390C94C01251B13D1A483D21E8B781D703D"
         , mnemonic = "hash0"
         , names =
             [ DigestName{marker = 0x00, mnemonic = "shake128", checkDigest = CheckShake128}
@@ -268,7 +270,7 @@ ctx0 = mkContext [hash0]
 foo :: NamespaceDefinition
 foo =
     NamespaceDefinition
-        { matchID = MatchQualifiedNamePrefix (Name (AssociatedNamespace hash0) 0x00) "\x11\x7A\x63\xBB\xF0\x4F\x4A\x57\x3C\x6D\x29\xE0\xD0\x32\x4F\x60\x46\xEF\xF3\x14\xB9\xBC\xD9\x65\x87\x2A\x04\x55\x97\x8C\xA2\x1B"
+        { matchID = MatchQualifiedNamePrefix (Name (AssociatedNamespace hash0) 0x00) $ fromHex "117A63BBF04F4A573C6D29E0D0324F6046EFF314B9BCD965872A0455978CA21B"
         , mnemonic = "foo"
         , names =
             [ SelfEval{marker = 0x00, mnemonic = "foo"}
@@ -278,7 +280,7 @@ foo =
 bar :: NamespaceDefinition
 bar =
     NamespaceDefinition
-        { matchID = MatchQualifiedNamePrefix (Name (AssociatedNamespace hash0) 0x00) "\x67\x44\xBA\x37\x14\x8D\x74\x5E\xE0\xB2\x57\x17\xA0\x12\xD1\xE2\x72\x76\x49\xC2\xBD\x28\x51\x98\xA5\x9A\x2D\xEE\x50\x65\x73\x91"
+        { matchID = MatchQualifiedNamePrefix (Name (AssociatedNamespace hash0) 0x00) $ fromHex "6744BA37148D745EE0B25717A012D1E2727649C2BD285198A59A2DEE50657391"
         , mnemonic = "bar"
         , names =
             [ SelfEval{marker = 0x00, mnemonic = "bar"}
@@ -302,3 +304,6 @@ eval' nss = eval (mkContext nss)
 
 matchTo :: (ToBULK a) => [(a, BULK)] -> IO ()
 matchTo = traverse_ (uncurry $ shouldBe . toBULK)
+
+fromHex :: Text -> ByteString
+fromHex = fromStrict . fromJust . H.decodeHex
