a8b93024bb1d5588144ba082d8da55702d1897f6
Pierre Thierry <pierre@nothos.net>
2026-02-03 18:01:38 +0100

    Better debug

--- src/Data/BULK/Debug.hs
+++ src/Data/BULK/Debug.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DefaultSignatures #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE RecordWildCards #-}
@@ -9,6 +10,7 @@ import Data.BULK.Eval.Types
 import Data.BULK.Types
 import Data.Bifunctor (bimap)
 import Data.ByteString.Lazy (ByteString)
+import Data.List (intercalate)
 import Data.Map qualified as M
 import Data.Set qualified as S
 import Data.String.Interpolate (i)
@@ -33,22 +35,32 @@ instance Debug ByteString where
 instance Debug MatchID where
     debug MatchNone = "!"
     debug (MatchEq bulk) = [i|== #{debug bulk}|]
-    debug (MatchNamePrefix num bs) = [i|#{num} <<< #{debug bs}|]
-    debug (MatchQualifiedNamePrefix _name bs) = [i|<<< #{debug bs}|]
+    debug (MatchNamePrefix num bs) = [i|#{num} (<<<) #{take 8 $ debug bs}|]
+    debug (MatchQualifiedNamePrefix _name bs) = [i|<<< #{take 8 $ debug bs}|]
 
 instance Debug Name where
-    debug (Name ns num) = [i|#{debug ns}:#{num}|]
+    debug (Name ns num) = [i|#{shortNS ns}:#{num}|]
 
 instance Debug Namespace where
     debug CoreNamespace = "{core}"
     debug (UnassociatedNamespace num) = [i|{#{num}}|]
-    debug (AssociatedNamespace ns) = [i|{NS #{debug ns}}|]
+    debug (AssociatedNamespace ns) = [i|#{debug ns}}|]
+
+shortNS :: Namespace -> String
+shortNS (AssociatedNamespace (NamespaceDefinition{mnemonic})) = [i|{#{mnemonic}}|]
+shortNS ns = debug ns
 
 instance Debug NamespaceDefinition where
-    debug NamespaceDefinition{..} = [i|#{mnemonic}(#{length names}, #{debug matchID})|]
+    debug NamespaceDefinition{..} = [i|#{mnemonic}(#{length names}, #{debug matchID}, #{map debug names})|]
+
+instance Debug NameDefinition where
+    debug SelfEval{marker, mnemonic} = [i|#{marker}:#{mnemonic}==|]
+    debug DigestName{marker, mnemonic, checkDigest} = [i|#{marker}:#{mnemonic}~~#{checkDigest}|]
+    debug ExpressionName{marker, mnemonic, expression} = [i|#{marker}:#{mnemonic}=#{debug expression}|]
+    debug LazyName{marker, mnemonic, lazyFunction} = [i|#{marker}:#{mnemonic}<-#{lazyFunction}|]
 
 instance Debug Package where
-    debug Package{..} = [i|{pkg (#{debug matchID}) #{map debug nsIDs}|]
+    debug Package{..} = [i|{pkg (#{debug matchID}) #{debug nsIDs}|]
 
 instance Debug Context where
     debug (Context scope) = debug scope
@@ -57,6 +69,9 @@ instance (Debug a) => Debug (Maybe a) where
     debug Nothing = "<>"
     debug (Just x) = [i|<#{debug x}>|]
 
+instance (Debug a) => Debug [a] where
+    debug as = "[" ++ intercalate ", " (map debug as) ++ "]"
+
 instance (Debug a, Debug b) => Debug (a, b) where
     debug (a, b) = [i|(#{debug a}, #{debug b})|]
 
@@ -64,6 +79,9 @@ instance (Debug k, Debug v) => Debug (M.Map k v) where
     debug = show . map (bimap debug debug) . M.toList
 
 instance Debug Int
+
+instance {-# OVERLAPPING #-} Debug [Char] where
+    debug = id
 
 instance (Debug v) => Debug (S.Set v) where
     debug = show . map debug . S.toList
