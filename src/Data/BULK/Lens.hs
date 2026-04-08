8270d7c9cb9e3b095d1b113681d5cb92bcc0e1dd
Pierre Thierry <pierre@nothos.net>
2026-04-09 00:50:50 +0200

    Remove unused lens that will create a dependency cycle

--- src/Data/BULK/Lens.hs
+++ src/Data/BULK/Lens.hs
@@ -7,20 +7,15 @@ module Data.BULK.Lens where
 
 import Control.Lens (Lens', Prism', Traversal', at, ix, lens, makeLenses, makePrisms, prism', (^?!))
 import Data.Bits (Bits)
-import Data.ByteString.Lazy (ByteString)
-import Data.Either.Extra (eitherToMaybe)
 import Data.Text (Text)
 
 import Data.BULK.Core (encodeInt, toIntegral)
 import Data.BULK.Decode (toNat)
 import Data.BULK.Encode (encodeNat)
-import Data.BULK.TextNotation (parseNotation)
 import Data.BULK.Types (BULK, CheckDigest, Context, Name (..), Namespace (..), NamespaceID, Ref (..), Scope, Value (..), withKey)
 import Data.BULK.Types qualified as Core
 import Data.Map.Strict qualified as M
 import Data.Word (Word8)
-import Polysemy (run)
-import Polysemy.Error (runError)
 
 makePrisms ''BULK
 makePrisms ''Context
@@ -33,9 +28,6 @@ _Nat = prism' encodeNat toNat
 -- | This 'Prism' provides a 'Traversal' for tweaking the integer encoded as a BULK expression
 _Int :: (Integral a) => Prism' BULK a
 _Int = prism' encodeInt toIntegral
-
-_Bytes :: Prism' Text ByteString
-_Bytes = prism' undefined $ eitherToMaybe . run . runError . parseNotation
 
 _Expression :: Prism' Value BULK
 _Expression = prism' Expression extract
